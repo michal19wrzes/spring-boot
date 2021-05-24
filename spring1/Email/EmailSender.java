c5a6149d35f6ee19d0bd2444f6b189fb1e9e14d3
michal19wrzes <michal19wrzes@gmail.com>
2021-05-24 20:03:22 +0200

    add confirmation email sending method

---
+++ spring1/Email/EmailSender.java
@@ -0,0 +1,5 @@
+package pl.test1.spring1.Email;
+
+public interface EmailSender {
+	void send(String to,String email);
+}
--- spring1/registration/RegistrationService.java
+++ spring1/registration/RegistrationService.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import lombok.AllArgsConstructor;
 import pl.test1.spring1.appUser.UserDetailsService;
+import pl.test1.spring1.Email.EmailSender;
 import pl.test1.spring1.appUser.User;
 import pl.test1.spring1.appUser.UserRole;
 import pl.test1.spring1.registration.token.ConfirmationToken;
@@ -19,14 +20,15 @@ public class RegistrationService {
 	private final UserDetailsService userDetailsService;
 	private final EmailValidator emailValidator;
 	private final ConfirmationTokenService confirmationTokenService;
+	private final EmailSender emailSender;
 
 	public String register(RegistrationRequest request) {
 		boolean isValidEmail = emailValidator.test(request.getEmail());
 		if(!isValidEmail) {
 			throw new IllegalStateException("Email is not valid");
 		}
-			
-		return userDetailsService.signUpUser(
+		
+		String token = userDetailsService.signUpUser(
 				new User(
 						request.getUserName(),
 						request.getLastName(),
@@ -34,8 +36,15 @@ public class RegistrationService {
 						request.getPassword(),
 						UserRole.USER
 				));
+		String link = "localhost:8080/api/v1/registration/confirm?token=" +token;
+		emailSender.send(
+				request.getEmail(),
+				buildEmail(request.getUserName(),link));
+		return token;
 	}
 	
+	
+
 	@Transactional
 	public String confirmToken(String token) {
 		ConfirmationToken confirmationToken = confirmationTokenService
@@ -56,6 +65,10 @@ public class RegistrationService {
 				confirmationToken.getUser().getEmail());
 		
 		return "confirmed";
+	}
+	private String buildEmail(String name, String link) {
+		// TODO template do zrobienia
+		return String.format("Confirm your account %s", link);
 	}
 
 }
